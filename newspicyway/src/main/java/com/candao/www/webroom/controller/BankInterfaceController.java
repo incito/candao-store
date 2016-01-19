4ba365b42ba2cb972bcb8ddb90d2489b60530b72
xiaokai365 <snail@snail-PC>
2016-01-19 16:52:37 +0800

    支持浦发银行

---
+++ newspicyway/src/main/java/com/candao/www/webroom/controller/BankInterfaceController.java
@@ -0,0 +1,36 @@
+package com.candao.www.webroom.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.candao.common.utils.JacksonJsonMapper;
+import com.candao.www.constant.Constant;
+import com.candao.www.webroom.service.DataDictionaryService;
+
+@Controller
+@RequestMapping("/bankinterface")
+public class BankInterfaceController {
+	@Autowired
+	DataDictionaryService dataDictionaryService;
+
+	/**
+	 * 返回所有的刷卡银行，POS调用
+	 * @return
+	 */
+	@RequestMapping("/getallbank")
+	@ResponseBody
+	public String getAllBank(){
+		try{
+			List<Map<String, Object>> datasByType = dataDictionaryService.getDatasByType("BANK");
+			return JacksonJsonMapper.objectToJson(datasByType);
+		}catch(Exception e){
+			return Constant.FAILUREMSG;
+		}
+	}
+
+}
