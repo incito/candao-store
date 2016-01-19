f0649070eeb26ab8eeb2bd75f2848a56ccb061f1
zhangjijun <sir.zhang@foxmail.com>
2016-01-19 16:45:53 +0800

    Revert "修改结算方式取值方法"
    This reverts commit 6ee8ab599cfc132aa898db3d55af5adff2f8698f.

--- newspicyway/src/main/java/com/candao/www/webroom/service/impl/SettlementOptionServiceImpl.java
+++ newspicyway/src/main/java/com/candao/www/webroom/service/impl/SettlementOptionServiceImpl.java
@@ -1,5 +1,7 @@
 package com.candao.www.webroom.service.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -26,13 +28,39 @@ public class SettlementOptionServiceImpl implements SettlementOptionService {
 	@Override
 	public List<Map<String, Object>> settlementOptionList(Map<String, Object> params) {
 		List<Map<String,Object>> settlement = tsettlementOptionDao.settlementOptionList(params);
-		return settlement;
+		List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+		for(Map<String,Object> map : settlement){
+			Map<String,Object> s = new HashMap<String,Object>();
+			String membercardno = String.valueOf(map.get("membercardno"));
+			String itemid = String.valueOf(map.get("itemid"));
+			String payway = String.valueOf(map.get("payway"));
+			s.put("payway",payway);
+			s.put("nums",map.get("nums"));
+			s.put("prices",map.get("prices"));
+			s.put("membercardno", membercardno);
+			s.put("itemid", itemid);
+			list.add(s);
+		}
+		return list;
 	}
 
 	@Override
 	public void exportXls(Map<String, Object> params, HttpServletRequest req, HttpServletResponse resp) throws Exception {
 		List<Map<String,Object>> settlementOptionList = tsettlementOptionDao.settlementOptionList(params);
 		String vasd ="结算方式明细表";
-		PoiExcleTest.exportExcleC(settlementOptionList,params, vasd, req,resp);
+        List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+        for (Map<String, Object> map : settlementOptionList){
+        	Map<String,Object> s = new HashMap<String,Object>();
+			String membercardno = String.valueOf(map.get("membercardno"));
+			String itemid = String.valueOf(map.get("itemid"));
+			String payway = String.valueOf(map.get("payway"));
+			s.put("payway",payway);
+			s.put("nums",map.get("nums"));
+			s.put("prices",map.get("prices"));
+			s.put("membercardno", membercardno);
+			s.put("itemid", itemid);
+			list.add(s);
+        }
+		PoiExcleTest.exportExcleC(list,params, vasd, req,resp);
 	}
 }
