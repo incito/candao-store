d2a266160ceebdcb28f25c7a2991042c7c0751cd
yintongqiang <261026193@qq.com>
2016-04-19 10:52:21 +0800

    update

--- newspicyway/src/main/java/com/candao/www/dataserver/util/DataServerJsonFormat.java
+++ newspicyway/src/main/java/com/candao/www/dataserver/util/DataServerJsonFormat.java
@@ -2,6 +2,7 @@ package com.candao.www.dataserver.util;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.candao.www.dataserver.model.ResponseData;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,23 +14,24 @@ import java.util.Map;
  */
 public class DataServerJsonFormat {
     public static String jsonFormat(Object object, String repStr) {
-        String jsonStr = "{\"Data\":" + JSON.toJSONString(object, SerializerFeature.WriteNullStringAsEmpty, SerializerFeature.WriteNullNumberAsZero) + "}";
+        String jsonStr = "{\"Data\":" + JSON.toJSONString(object, SerializerFeature.WriteMapNullValue, SerializerFeature.WriteNullStringAsEmpty, SerializerFeature.WriteNullNumberAsZero) + "}";
         return jsonStr.replace("\"", repStr);
     }
 
     public static String jsonFormat(Object object) {
-        return "{\"Data\":" + JSON.toJSONString(object, SerializerFeature.WriteNullStringAsEmpty, SerializerFeature.WriteNullNumberAsZero) + "}";
+        return "{\"Data\":" + JSON.toJSONString(object, SerializerFeature.WriteMapNullValue, SerializerFeature.WriteNullStringAsEmpty, SerializerFeature.WriteNullNumberAsZero) + "}";
     }
 
     public static void main(String[] args) {
         List<Map> mapList = new ArrayList<>();
-//        Map map = new HashMap();
-//        Map map1 = new HashMap();
-//        map.put("131", 2132);
-//        map1.put("131", "2132");
-//        mapList.add(map);
-//        mapList.add(map1);
+        Map map = new HashMap();
+        Map map1 = new HashMap();
+        map.put("131", 2132);
+        map1.put("132", null);
+        mapList.add(map);
+        mapList.add(map1);
         System.out.println(DataServerJsonFormat.jsonFormat(mapList, "|"));
-        System.out.println(DataServerJsonFormat.jsonFormat(mapList, ""));
+        System.out.println(DataServerJsonFormat.jsonFormat(new ResponseData(), "|"));
+        System.out.println(DataServerJsonFormat.jsonFormat(mapList));
     }
 }
