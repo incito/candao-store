dcaf232877654c147c7c26fb97f9f8ff9fa8dda7
xiaokai365 <snail@snail-PC>
2016-04-21 11:00:05 +0800

    重复下单提交

--- newspicyway/sql_script/7-分店-procedure-event-view-other.sql
+++ newspicyway/sql_script/7-分店-procedure-event-view-other.sql
@@ -2580,3 +2580,60 @@ BEGIN
     end if;
 END
 $$
+
+DROP PROCEDURE IF EXISTS p_update_primarykey$$
+CREATE PROCEDURE p_update_primarykey(in primarykey varchar(100))
+COMMENT '更新重复的primarykey'
+
+BEGIN
+
+DECLARE done int DEFAULT 0;
+
+DECLARE v_detail_id varchar(100);
+
+DECLARE  temp_uuid varchar(100);
+
+DECLARE cur_detailids CURSOR FOR SELECT orderdetailid from t_order_detail d where  d.primarykey=primarykey;
+
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+  OPEN cur_detailids;
+  REPEAT
+    FETCH cur_detailids INTO v_detail_id;
+				IF done <> 1 THEN
+								SELECT  UUID() into temp_uuid;
+								UPDATE t_order_detail set primarykey=temp_uuid where orderdetailid=v_detail_id;
+				 END IF;
+     UNTIL done = 1
+  END REPEAT;
+
+END
+$$
+
+DROP PROCEDURE IF EXISTS p_get_primarykeys$$
+CREATE PROCEDURE p_get_primarykeys()
+COMMENT '获取重复的primarykey'
+
+BEGIN
+DECLARE done int DEFAULT 0;
+DECLARE  v_total varchar(100);
+DECLARE  v_primarykey varchar(100);
+
+--###定义一个游标表示所有重复的primarykey
+DECLARE cur_repat_keys CURSOR FOR SELECT count(1) total,t.primarykey
+			                          from t_order_detail t
+	                                   GROUP BY t.primarykey
+					                    HAVING total>1
+			                          ORDER BY total desc;
+--##先更新以前没有用到primarykey字段的数据
+update t_order_detail set primarykey=UUID() where primarykey='';
+OPEN cur_repat_keys;
+  REPEAT
+    FETCH cur_repat_keys INTO v_total,v_primarykey;
+				IF done <> 1 THEN
+				call p_update_primarykey(v_primarykey);
+				END IF;
+  UNTIL done = 1
+  END REPEAT;
+
+END
+$$
