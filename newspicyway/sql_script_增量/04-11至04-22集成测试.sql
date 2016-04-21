10c470f3c4168c0df767f0847a5de9499bf1825c
xiaozei523@163.com <xiaozei523@163.com>
2016-04-21 16:19:53 +0800

    增加 本次集成测试的sql文件

---
+++ newspicyway/sql_script_增量/04-11至04-22集成测试.sql
@@ -0,0 +1,13 @@
+﻿--
+DROP TABLE IF EXISTS `t_b_tip`;
+
+CREATE TABLE `t_b_tip` (
+  `waiter_number` varchar(50) NOT NULL COMMENT '服务员编号',
+  `receivables` int(11) NOT NULL COMMENT '应收金额',
+  `paid` int(11) DEFAULT NULL COMMENT '实收金额',
+  `orderid` varchar(50) NOT NULL COMMENT '订单编号',
+  `insertime` datetime NOT NULL COMMENT '创建时间',
+  `branchid` int(11) NOT NULL COMMENT '门店编号',
+  PRIMARY KEY (`orderid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
