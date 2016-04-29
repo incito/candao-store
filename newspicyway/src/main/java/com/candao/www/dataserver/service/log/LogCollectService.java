c59d7647713f74cc52e33987d748db6a76a8de6b
yintongqiang <261026193@qq.com>
2016-04-29 14:41:03 +0800

    机具日志处理

--- newspicyway/src/main/java/com/candao/www/dataserver/service/log/LogCollectService.java
+++ newspicyway/src/main/java/com/candao/www/dataserver/service/log/LogCollectService.java
@@ -3,7 +3,6 @@ package com.candao.www.dataserver.service.log;
 import com.alibaba.fastjson.JSON;
 import com.candao.www.dataserver.model.LogCollectData;
 import com.candao.www.dataserver.model.MsgForwardData;
-import com.candao.www.dataserver.model.ReConnectData;
 import com.candao.www.dataserver.model.ResponseData;
 import com.candao.www.dataserver.service.device.obj.DeviceObject;
 import com.candao.www.dataserver.service.msghandler.MsgForwardService;
@@ -23,13 +22,13 @@ import java.util.Map;
  */
 @Service
 public class LogCollectService implements MsgHandler {
-    private final static org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger("device_error");
+    private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(LogCollectService.class);
     @Autowired
     private MsgForwardService msgForwardService;
 
     @Override
     public void handler(DeviceObject deviceObject, String serialNumber, String msg) {
-        LOGGER.error(msg);
+        LOGGER.info("######device-log msg=" + msg);
         final LogCollectData logCollectData = MsgAnalyzeTool.analyzeToLogCoData(msg);
         Map<String, List<String>> target = new HashMap<>();
         target.put(logCollectData.getGroup(), new ArrayList<String>() {{
