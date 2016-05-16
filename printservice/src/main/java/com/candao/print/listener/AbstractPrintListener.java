bbd12c8766c09894b96ad1a2934921df6760bd9b
zhangjijun <sir.zhang@foxmail.com>
2016-05-16 16:30:05 +0800

    自动返回打印状态BUG修改；主打印机状态异常丢单的BUG修改

--- printservice/src/main/java/com/candao/print/listener/AbstractPrintListener.java
+++ printservice/src/main/java/com/candao/print/listener/AbstractPrintListener.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.SocketException;
 
 import javax.jms.Destination;
 
@@ -32,7 +32,7 @@ public abstract class AbstractPrintListener implements PrintListener {
 	/**
 	 * 打印机返回正常的状态
 	 */
-	private static final String NORMAL = "10110";
+	private static final String NORMAL = "10100^0^0^1111^";
 
 	Log logger = LogFactory.getLog(AbstractPrintListener.class);
 
@@ -97,20 +97,19 @@ public abstract class AbstractPrintListener implements PrintListener {
 	 * @param socketOut
 	 * @param socket
 	 * @param object
-	 * @throws IOException
 	 */
-	private boolean checkPrinter(OutputStream socketOut, final Socket socket) throws IOException {
+	private boolean checkPrinter(OutputStream socketOut, final Socket socket) {
 		String hostAddress = socket.getInetAddress().getHostAddress();
 		InputStream inputStream;
-		socketOut.write(new byte[] { 16, 4, 1 });
-		byte[] rs = new byte[1];
+		byte[] rs = new byte[4];
 		try {
+			socketOut.write(new byte[] { 29, 97, 1 });
 			inputStream = socket.getInputStream();
 			inputStream.read(rs);
 
 			String rs_str = "";
 			for (byte b : rs) {
-				rs_str += Integer.toBinaryString(b);
+				rs_str += Integer.toBinaryString(b) + "^";
 			}
 			if (!rs_str.equals(NORMAL)) {
 				logger.error("打印机状态异常，IP：" + hostAddress + "，状态码：" + rs_str, null);
@@ -140,22 +139,25 @@ public abstract class AbstractPrintListener implements PrintListener {
 	}
 
 	private void printForm(PrintObj object, String ipstr, String ipAddress, boolean isMainPrint) {
+		OutputStream socketOut = null;
+		OutputStreamWriter writer = null;
+		Socket socket = null;
 		try {
 			if (object != null && listenerID != null && !"".equals(listenerID)) {
 				logger.info("------------------------");
 				logger.info(listenerID + " 收到打印对象:\n" + JacksonJsonMapper.objectToJson(object));
 			}
-			OutputStream socketOut;
-			OutputStreamWriter writer;
-			Socket socket;
 			int print_port;
 			print_port = Integer.parseInt(object.getCustomerPrinterPort());
 
-			socket = new Socket(ipAddress, print_port);
+			socket = new Socket();
+			socket.connect(new InetSocketAddress(ipAddress, print_port), 5000);//建立连接5秒超时
+			socket.setKeepAlive(true);  
+	        socket.setSoTimeout(5 * 1000);      //从inputStream中读打印机状态返回值的超时时间
 			socketOut = socket.getOutputStream();
 			writer = new OutputStreamWriter(socketOut, Constant.PRINTERENCODE);
 
-			boolean flag = "Y".equals(PropertiesUtils.getValue(Constant.CHECKSTATE).trim().toUpperCase());
+			boolean flag = PropertiesUtils.getValue(Constant.CHECKSTATE) == null || "Y".equals(PropertiesUtils.getValue(Constant.CHECKSTATE).trim().toUpperCase());
 			
 			//打印之前检查打印机状态
 			if(flag){
@@ -176,7 +178,7 @@ public abstract class AbstractPrintListener implements PrintListener {
 
 			//调用各监听器的实现类打印具体的内容
 			printBusinessData(object, socketOut, writer);
-
+			
 			// 下面指令为打印完成后自动走纸
 			writer.write(27);
 			writer.write(100);
@@ -185,22 +187,17 @@ public abstract class AbstractPrintListener implements PrintListener {
 			writer.flush();//
 			socketOut.write(new byte[] { 0x1B, 0x69 });// 切纸
 
-			if(flag){
-				//打印完成后检查打印机状态
-				boolean checkPrinter = checkPrinter(socketOut, socket);
-				if(!checkPrinter){
-					resolve(object, ipstr, ipAddress, isMainPrint);
-				}				
-			}
+//			if(flag){
+//				//打印完成后检查打印机状态
+//				boolean checkPrinter = checkPrinter(socketOut, socket);
+//				if(!checkPrinter){
+//					resolve(object, ipstr, ipAddress, isMainPrint);
+//				}				
+//			}
 
-			//关闭连接
-			writer.close();
-			socketOut.close();
-			socket.close();
-
-		} catch (SocketException se) {
+		} catch (IOException se) {
 			logger.error("------------------------", null);
-			logger.error("打印异常(socket异常)，订单号：" + object.getOrderNo() + se.getMessage(), se);
+			logger.error("打印异常(socket异常)，ip："+ipAddress+"，订单号：" + object.getOrderNo() + se.getMessage(), se);
 			if (isMainPrint && hasBackupPrinter(ipstr)) {
 				// 备用机打印
 				ipAddress = getBackupPrinter(ipstr);
@@ -215,7 +212,28 @@ public abstract class AbstractPrintListener implements PrintListener {
 			logger.error("------------------------", null);
 			logger.error("打印异常，订单号：" + object.getOrderNo() + e.getMessage(), e);
 		} finally {
-			
+			//关闭连接
+			if(writer != null){
+				try {
+					writer.close();
+				} catch (IOException e) {
+					logger.error("关闭writer失败，IP:" + ipAddress, e);
+				}
+			}
+			if(socketOut != null){
+				try {
+					socketOut.close();
+				} catch (IOException e) {
+					logger.error("关闭socketOut失败，IP:" + ipAddress, e);
+				}
+			}
+			if(socket != null){
+				try {
+					socket.close();
+				} catch (IOException e) {
+					logger.error("关闭socket失败，IP:" + ipAddress, e);
+				}
+			}
 		}
 	}
 
@@ -236,9 +254,12 @@ public abstract class AbstractPrintListener implements PrintListener {
 				//没有配置备用打印机，但主打印机状态异常
 				logger.error("------------------------", null);
 				logger.error("打印机状态异常，IP:" + ipAddress + ",订单号：" + object.getOrderNo(), null);
+				joinExceptionQueue(object, listenerID);
 			}
 		} else {
 			//备用打印机状态异常，则加入异常队列
+			logger.error("------------------------", null);
+			logger.error("备用打印机状态异常，IP:" + ipAddress + ",订单号：" + object.getOrderNo(), null);
 			joinExceptionQueue(object, listenerID);
 		}
 	}
