804efed13dffad2b9f0486b8d1e979b022c26798
liaoying <360858909@qq.com>
2016-06-02 17:19:51 +0800

    2

--- newspicyway/src/main/java/com/candao/www/webroom/controller/ControllerController.java
+++ newspicyway/src/main/java/com/candao/www/webroom/controller/ControllerController.java
@@ -3,9 +3,12 @@ package com.candao.www.webroom.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -18,14 +21,23 @@ public class ControllerController {
     private Logger logger = LoggerFactory.getLogger(ControllerController.class);
 
     /**
-     * 返回所有的刷卡银行，POS调用
+     * 重启dataserver
      *
      * @return
      */
     @RequestMapping("/restartDataserver")
     @ResponseBody
     public String restartDataServer() {
-        exec("");
+        String cmd;
+        try {
+            File batFile = ResourceUtils.getFile("classpath:other/DataServerRestart.bat");
+            cmd = batFile.getAbsolutePath();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            logger.error("获取dataserver重启bat失败", e);
+            return "1";
+        }
+        exec(cmd);
         return "0";
     }
 
@@ -45,5 +57,10 @@ public class ControllerController {
             e.printStackTrace();
         }
         System.out.println("child thread done");
+    }
+
+    public static void main(String[] args) {
+        ControllerController c = new ControllerController();
+        c.restartDataServer();
     }
 }
