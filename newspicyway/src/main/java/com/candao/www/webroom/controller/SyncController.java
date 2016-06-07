3944ac3d33136400457d0c943b37477d83ed0f8c
xiaozei523@163.com <xiaozei523@163.com>
2016-06-07 22:24:54 +0800

    合入 数据下发功能

---
+++ newspicyway/src/main/java/com/candao/www/webroom/controller/SyncController.java
@@ -0,0 +1,72 @@
+package com.candao.www.webroom.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.candao.common.utils.HttpUtils;
+import com.candao.common.utils.JacksonJsonMapper;
+import com.candao.common.utils.PropertiesUtils;
+import com.candao.www.webroom.service.impl.SyncService;
+
+/**
+ * 门店与总店 数据同步功能
+ *
+ * @author zgt
+ */
+@Controller
+@RequestMapping("/sync")
+public class SyncController {
+  private Logger logger = LoggerFactory.getLogger(SyncController.class);
+  @Autowired
+  private SyncService syncservice;
+
+  /**
+   * @return String 同步结果
+   * @Description:分店拉取总店数据
+   * @Modification:
+   */
+  @RequestMapping(value = "/synDataFromCloud")
+  @ResponseBody
+  public Map<String, Object> synDataFromCloud(String type) {
+    Map<String, Object> resp = new HashMap<>();
+    String url = "http://" + PropertiesUtils.getValue("cloud.host") + "/" + PropertiesUtils.getValue("cloud.webroot") + "/sync/dish";
+    String queryString = "tenantId=" + PropertiesUtils.getValue("tenant_id") + "&branchId=" + PropertiesUtils.getValue("current_branch_id") + "&type=" + type;
+    String result = HttpUtils.doGet(url, queryString);
+    Map<String, Object> map = null;
+
+    if ("Read timed out".equalsIgnoreCase(result)) {
+      logger.error("从总店同步数据超时：" + url + "?" + queryString);
+      resp.put("statusCode", "500");
+      resp.put("data", "从总店获取数据超时");
+      return resp;
+    }
+
+    try {
+      map = JacksonJsonMapper.jsonToObject(result, Map.class);
+    } catch (Exception e) {
+      logger.error("从总店同步数据失败\n" + result);
+      resp.put("statusCode", "500");
+      resp.put("data", "从总店获取数据失败");
+      return resp;
+    }
+
+    try {
+      syncservice.saveDish((String) map.get("sql"), (List) map.get("tableNames"));
+      resp.put("statusCode", "200");
+    } catch (Exception e) {
+      logger.error("从总店同步数据异常", e);
+      resp.put("statusCode", "500");
+      resp.put("data", "保存数据失败");
+    }
+    return resp;
+  }
+
+}
---
+++ newspicyway/src/main/java/com/candao/www/webroom/service/impl/SyncService.java
@@ -0,0 +1,72 @@
+package com.candao.www.webroom.service.impl;
+
+import com.candao.common.dao.DaoSupport;
+import com.candao.www.spring.SpringContext;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.util.StringUtils;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+/**
+ * 门店与总店数据同步功能
+ *
+ * @author zgt
+ */
+@Service
+public class SyncService {
+  @Autowired
+  private DaoSupport dao;
+  @Autowired
+  @Qualifier("sqlSessionFactory")
+  private SqlSessionFactory sessionFactory;
+
+  public void saveDish(String sql, List<String> tableNames) {
+
+    DataSourceTransactionManager transactionManager = (DataSourceTransactionManager) SpringContext.getBean(DataSourceTransactionManager.class);
+    DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
+    definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+    TransactionStatus status = transactionManager.getTransaction(definition);
+    try (
+        Connection connection = sessionFactory.openSession().getConnection();
+        Statement stmt = connection.createStatement()
+    ) {
+
+      connection.setAutoCommit(false);
+
+      for (String tableName : tableNames) {
+        if ("t_dictionary".equalsIgnoreCase(tableName))
+          stmt.addBatch("DELETE FROM t_dictionary WHERE `type` IN ('SPECIAL','DISHSTATUS')");
+        else
+          stmt.addBatch("DELETE FROM " + tableName);
+      }
+
+      String[] sqls = sql.split(";");
+      for (String s : sqls) {
+        if (StringUtils.hasText(s))
+          stmt.addBatch(s);
+      }
+
+      try {
+        stmt.executeBatch();
+        transactionManager.commit(status);
+      } catch (SQLException e) {
+        transactionManager.rollback(status);
+        throw e;
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+
+}
