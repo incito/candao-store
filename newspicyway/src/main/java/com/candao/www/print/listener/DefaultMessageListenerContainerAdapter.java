1d2369b68647e770db270e260f847ef57440ab21
caicai <837522223@qq.com>
2016-06-13 16:36:28 +0800

    根据打印机ip创建队列

---
+++ newspicyway/src/main/java/com/candao/www/print/listener/DefaultMessageListenerContainerAdapter.java
@@ -0,0 +1,19 @@
+package com.candao.www.print.listener;
+
+import org.springframework.jms.listener.DefaultMessageListenerContainer;
+
+/**
+ * Created by Administrator on 2016-6-12.
+ */
+public class DefaultMessageListenerContainerAdapter extends DefaultMessageListenerContainer {
+
+    @Override
+    public void start() {
+    }
+
+    public void trulyStart() {
+        super.start();
+    }
+
+
+}
