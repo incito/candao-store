231892f0f6e83e73e57855393d81f153d23bab94
liaoying <360858909@qq.com>
2016-06-14 16:39:43 +0800

    打印

--- printservice/src/main/java/com/candao/print/v2/PrinterManager.java
+++ printservice/src/main/java/com/candao/print/v2/PrinterManager.java
@@ -1,8 +1,12 @@
 package com.candao.print.v2;
 
+import com.candao.print.service.PrinterService;
+import com.candao.print.utils.SpringContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -26,39 +30,39 @@ public class PrinterManager {
     }
 
     private static void initialize() {
-        PrinterService printerService= (PrinterService) SpringContext.getBean(PrinterService.class);
+        PrinterService printerService = (PrinterService) SpringContext.getBean(PrinterService.class);
         List<Map<String, Object>> printerList = printerService.find(null);
-        if(null!=printerList){
-            for(Map<String,Object> printer:printerList){
+        if (null != printerList) {
+            for (Map<String, Object> printer : printerList) {
                 Object ipaddress = printer.get("ipaddress");
                 Object port = printer.get("port");
-                if(StringUtils.isEmpty(ipaddress)||StringUtils.isEmpty(port)){
+                if (StringUtils.isEmpty(ipaddress) || StringUtils.isEmpty(port)) {
                     continue;
                 }
 
                 String ipStr = ipaddress.toString();
-                int portInt=0;
+                int portInt = 0;
                 try {
                     portInt = Integer.parseInt(port.toString());
-                }catch (Exception e){
-                    logger.info("["+ipStr+"]端口不正常:"+port);
+                } catch (Exception e) {
+                    logger.info("[" + ipStr + "]端口不正常:" + port);
                     continue;
                 }
 
-                Printer p=new Printer();
+                Printer p = new Printer();
                 //备用打印机处理
-                String[] ips= ipStr.split(":");
+                String[] ips = ipStr.split(":");
                 p.setKey(ips[0]);
                 p.setIp(ips[0]);
                 p.setPort(portInt);
-                if(ips.length>1){
-                    Printer backPrinter=new Printer();
+                if (ips.length > 1) {
+                    Printer backPrinter = new Printer();
                     backPrinter.setKey(ips[1]);
                     backPrinter.setIp(ips[1]);
                     backPrinter.setPort(portInt);
                     p.setBackPrinter(backPrinter);
                 }
-                printers.put(p.getKey(),p);
+                printers.put(p.getKey(), p);
             }
         }
 //        // TODO: 2016/6/14 测试代码
