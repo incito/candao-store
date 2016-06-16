6f7cc5baf291a9128adcdb0a3bce4e0c8919d0a3
liaoying <360858909@qq.com>
2016-06-16 12:49:09 +0800

    监听打印机配置修改

--- newspicyway/src/main/java/com/candao/www/printer/v2/PrinterManager.java
+++ newspicyway/src/main/java/com/candao/www/printer/v2/PrinterManager.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 
+import java.net.Socket;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -16,7 +18,14 @@ import java.util.concurrent.ConcurrentHashMap;
 public class PrinterManager {
     private static Logger logger = LoggerFactory.getLogger(PrinterManager.class);
     private static Map<String, Printer> printers = new ConcurrentHashMap<>();
+    private static PrinterService printerService;
 
+    /**
+     * 获取打印机实例
+     *
+     * @param ipAddr
+     * @return
+     */
     public static Printer getPrinter(String ipAddr) {
         if (printers.isEmpty()) {
             synchronized (PrinterManager.class) {
@@ -29,10 +38,37 @@ public class PrinterManager {
         return printers.get(ipAddr);
     }
 
-    private static void initialize() {
+    /**
+     * 重新初始化打印机
+     *
+     * @param printerAddress
+     */
+    public synchronized static void initialize(List<String> printerAddress) {
+        logger.info("重新初始化打印机");
+        clearPrinter();
         PrinterService printerService = (PrinterService) SpringContext.getBean(PrinterService.class);
-        List<Map<String, Object>> printerList = printerService.find(null);
         printerService.clearWorkStatus();
+        if (null == printerAddress) {
+            return;
+        }
+        for (String address : printerAddress) {
+            String[] ipAndPort = address.split(":");
+            String ip = ipAndPort[0];
+            int port = 0;
+            try {
+                port = Integer.valueOf(ipAndPort[1]);
+            } catch (Exception e) {
+                logger.info("[" + ip + "]端口不正常:" + port);
+                continue;
+            }
+            createPrinter(ip, port);
+        }
+    }
+
+    private static void initialize() {
+        PrinterService printerService = getPrinterService();
+        printerService.clearWorkStatus();
+        List<Map<String, Object>> printerList = printerService.find(null);
         if (null != printerList) {
             for (Map<String, Object> printer : printerList) {
                 Object ipaddress = printer.get("ipaddress");
@@ -50,22 +86,7 @@ public class PrinterManager {
                     continue;
                 }
 
-                String[] ips = ipStr.split(",");
-                int length = ips.length > 2 ? 2 : ips.length;
-                for (int i = 0; i < length; i++) {
-                    String ip = ips[i];
-                    Printer p = hasCreated(ip);
-                    //如果该打印机还不存在，创建
-                    if (null == p) {
-                        p = new Printer();
-                        p.setKey(ips[0]);
-                        p.setIp(ips[0]);
-                        p.setPort(portInt);
-                        printers.put(p.getKey(), p);
-                        //初始化打印机状态
-                        printerService.updateWorkState(p.getIp(), PrinterStatusManager.NORMAL);
-                    }
-                }
+                createPrinter(ipStr, portInt);
             }
         }
 //        // TODO: 2016/6/14 测试代码
@@ -75,6 +96,25 @@ public class PrinterManager {
 //        p.setIp("10.66.18.3");
 //        p.setPort(9100);
 //        printers.put(p.getKey(),p);
+    }
+
+    private static void createPrinter(String ipStr, int portInt) {
+        String[] ips = ipStr.split(",");
+        int length = ips.length > 2 ? 2 : ips.length;
+        for (int i = 0; i < length; i++) {
+            String ip = ips[i];
+            Printer p = hasCreated(ip);
+            //如果该打印机还不存在，创建
+            if (null == p) {
+                p = new Printer();
+                p.setKey(ips[0]);
+                p.setIp(ips[0]);
+                p.setPort(portInt);
+                printers.put(p.getKey(), p);
+                //初始化打印机状态
+                getPrinterService().updateWorkState(p.getIp(), PrinterStatusManager.NORMAL);
+            }
+        }
     }
 
     /**
@@ -99,5 +139,32 @@ public class PrinterManager {
         for (Printer printer : printers.values()) {
             printer.checkState();
         }
+    }
+
+    private static PrinterService getPrinterService() {
+        if (null == printerService) {
+            printerService = (PrinterService) SpringContext.getBean(PrinterService.class);
+        }
+        return printerService;
+    }
+
+    private static void clearPrinter() {
+        logger.info("清空打印机集合");
+        for (Printer printer : printers.values()) {
+            Socket socket = printer.getChannel();
+            if (null != socket) {
+                try {
+                    socket.close();
+                } catch (Exception e) {
+
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("10.66.18.3,12.22.01.1:9100");
+        initialize(list);
     }
 }
