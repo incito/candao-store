a8795cbcdd9b8a067038c6c5b9092bf08bafab1c
liaoying <360858909@qq.com>
2016-08-03 09:37:02 +0800

    POS打印转后台

---
+++ printservice/src/main/java/com/candao/print/entity/ResultTip4Pos.java
@@ -0,0 +1,43 @@
+package com.candao.print.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class ResultTip4Pos implements Serializable {
+    private String resut;
+    private TimeInfo time;
+    private String mag;
+    private List<TipItem> data;
+
+    public String getResut() {
+        return resut;
+    }
+
+    public void setResut(String resut) {
+        this.resut = resut;
+    }
+
+    public TimeInfo getTime() {
+        return time;
+    }
+
+    public void setTime(TimeInfo time) {
+        this.time = time;
+    }
+
+    public String getMag() {
+        return mag;
+    }
+
+    public void setMag(String mag) {
+        this.mag = mag;
+    }
+
+    public List<TipItem> getData() {
+        return data;
+    }
+
+    public void setData(List<DishItem> data) {
+        this.data = data;
+    }
+}
---
+++ printservice/src/main/java/com/candao/print/entity/TipItem.java
@@ -0,0 +1,34 @@
+package com.candao.print.entity;
+
+/**
+ * Created by liaoy on 2016/8/3.
+ */
+public class TipItem {
+    private String waiterName;
+    private String serviceCount;
+    private String tipMoney;
+
+    public String getWaiterName() {
+        return waiterName;
+    }
+
+    public void setWaiterName(String waiterName) {
+        this.waiterName = waiterName;
+    }
+
+    public String getServiceCount() {
+        return serviceCount;
+    }
+
+    public void setServiceCount(String serviceCount) {
+        this.serviceCount = serviceCount;
+    }
+
+    public String getTipMoney() {
+        return tipMoney;
+    }
+
+    public void setTipMoney(String tipMoney) {
+        this.tipMoney = tipMoney;
+    }
+}
