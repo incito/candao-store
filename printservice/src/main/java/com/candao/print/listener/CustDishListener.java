3efac7f52710cfbe764bf2867683aa52a7342ee3
liaoying <360858909@qq.com>
2016-08-04 15:37:23 +0800

    结业接口去掉不必要的参数；订单打印日志增加打印机IP信息

--- printservice/src/main/java/com/candao/print/listener/CustDishListener.java
+++ printservice/src/main/java/com/candao/print/listener/CustDishListener.java
@@ -1,129 +1,127 @@
 package com.candao.print.listener;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-
 import com.candao.common.utils.StringUtils;
 import com.candao.print.entity.PrintData;
 import com.candao.print.entity.PrintDish;
 import com.candao.print.entity.PrintObj;
 import com.candao.print.entity.PrinterConstant;
 import com.candao.print.listener.template.ListenerTemplate;
- 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
 
 @Service
-public class CustDishListener extends AbstractQueueListener{
-	private Logger logger= LoggerFactory.getLogger(CustDishListener.class);
+public class CustDishListener extends AbstractQueueListener {
+    private Logger logger = LoggerFactory.getLogger(CustDishListener.class);
 
-	/**
-	 * 
-	 * @param message
-	 * @return
-	 */
-	public String receiveMessage(String message) {
-		return null;
-	}
-	
-	@Override
-	protected void printBusinessData(PrintObj object, PrintData socketOut, PrintData writer, ListenerTemplate template)
-			throws Exception {
-		System.out.println("CustDishListener receive message");
+    /**
+     * @param message
+     * @return
+     */
+    public String receiveMessage(String message) {
+        return null;
+    }
 
-		String billName = object.getBillName();
-		List<PrintDish> printDishList = object.getList();
+    @Override
+    protected void printBusinessData(PrintObj object, PrintData socketOut, PrintData writer, ListenerTemplate template)
+            throws Exception {
+        System.out.println("CustDishListener receive message");
 
-		socketOut.write(PrinterConstant.getFdDoubleFont());
-		// 居中
-		socketOut.write(template.setAlignCenter());
-		// 单号
-		writer.write(StringUtils.bSubstring2(billName, billName.length()));
-		writer.write("\r\n");
-		
+        String billName = object.getBillName();
+        List<PrintDish> printDishList = object.getList();
+
+        socketOut.write(PrinterConstant.getFdDoubleFont());
+        // 居中
+        socketOut.write(template.setAlignCenter());
+        // 单号
+        writer.write(StringUtils.bSubstring2(billName, billName.length()));
+        writer.write("\r\n");
+
 //		Object[] portName = template.getPrinterPortMsg(object);
 //		this.write(writer, portName);
 
-		writer.flush();
-		// 居中
-		socketOut.write(template.setAlignLeft());
-		socketOut.write(template.setClearfont());
+        writer.flush();
+        // 居中
+        socketOut.write(template.setAlignLeft());
+        socketOut.write(template.setClearfont());
 
-		writer.write("==========================================\r\n");
+        writer.write("==========================================\r\n");
 
-		String[] name = { object.getOrderNo(), object.getTimeMsg().substring(0, 10) };
-		// 最多显示34个字符
-		Integer[] len = { 22, 10 };
-		String[] header = StringUtils.getLineFeedText(name, len);
-		if (header != null) {
-			for (int i = 0; i < header.length; i++) {
-				if (i == 0) {
-					writer.write(StringUtils.bSubstring2("账单号:", 4) + header[i] + "\r\n");
-				} else {
-					writer.write(StringUtils.getStr(7) + header[i] + "\r\n");
-				}
-			}
-		}
+        String[] name = {object.getOrderNo(), object.getTimeMsg().substring(0, 10)};
+        // 最多显示34个字符
+        Integer[] len = {22, 10};
+        String[] header = StringUtils.getLineFeedText(name, len);
+        if (header != null) {
+            for (int i = 0; i < header.length; i++) {
+                if (i == 0) {
+                    writer.write(StringUtils.bSubstring2("账单号:", 4) + header[i] + "\r\n");
+                } else {
+                    writer.write(StringUtils.getStr(7) + header[i] + "\r\n");
+                }
+            }
+        }
 
-		String[] username = { object.getUserName(), object.getTableArea(), object.getTimeMsg().substring(11) };
-		Integer[] length = { 12, 10, 8 };
-		String[] body = StringUtils.getLineFeedText(username, length);
-		if (body != null) {
-			for (int i = 0; i < body.length; i++) {
-				if (i == 0) {
-					writer.write(StringUtils.bSubstring2("服务员:", 4) + body[i] + "\r\n");
-				} else {
-					writer.write(StringUtils.getStr(7) + body[i] + "\r\n");
-				}
-			}
-		}
+        String[] username = {object.getUserName(), object.getTableArea(), object.getTimeMsg().substring(11)};
+        Integer[] length = {12, 10, 8};
+        String[] body = StringUtils.getLineFeedText(username, length);
+        if (body != null) {
+            for (int i = 0; i < body.length; i++) {
+                if (i == 0) {
+                    writer.write(StringUtils.bSubstring2("服务员:", 4) + body[i] + "\r\n");
+                } else {
+                    writer.write(StringUtils.getStr(7) + body[i] + "\r\n");
+                }
+            }
+        }
 
-		writer.write("------------------------------------------\r\n");
+        writer.write("------------------------------------------\r\n");
 
-		writer.flush();//
+        writer.flush();//
 
-		// -------------------------------分割点-----------------------------------
-		socketOut.write(template.getBodyFont());
+        // -------------------------------分割点-----------------------------------
+        socketOut.write(template.getBodyFont());
 
-		// 居中
-		socketOut.write(template.setAlignCenter());
-		String[] table = template.getTableMsg(object);
-		this.write(writer, table);
-		// 左对齐
-		socketOut.write(template.setAlignLeft());
+        // 居中
+        socketOut.write(template.setAlignCenter());
+        String[] table = template.getTableMsg(object);
+        this.write(writer, table);
+        // 左对齐
+        socketOut.write(template.setAlignLeft());
 
-		writer.flush();//
-		socketOut.write(PrinterConstant.getClear_font());
-		writer.write("------------------------------------------\r\n");
-		writer.write(StringUtils.bSubstring2("品项 ", 13) + StringUtils.bSubstring2("数量", 4)
-				+ StringUtils.bSubstring2("单价", 2) + "\r\n");
-		writer.flush();//
+        writer.flush();//
+        socketOut.write(PrinterConstant.getClear_font());
+        writer.write("------------------------------------------\r\n");
+        writer.write(StringUtils.bSubstring2("品项 ", 13) + StringUtils.bSubstring2("数量", 4)
+                + StringUtils.bSubstring2("单价", 2) + "\r\n");
+        writer.flush();//
 
-		socketOut.write(template.getBodyFont());
+        socketOut.write(template.getBodyFont());
 
-		Object[] text = template.getBodyMsg(object);
+        Object[] text = template.getBodyMsg(object);
 
-		for (int i = 0; i < text.length; i++) {
-			writer.write(text[i].toString() + "\r\n");
-		}
+        for (int i = 0; i < text.length; i++) {
+            writer.write(text[i].toString() + "\r\n");
+        }
 
-		writer.flush();//
-		socketOut.write(PrinterConstant.getClear_font());
+        writer.flush();//
+        socketOut.write(PrinterConstant.getClear_font());
 
-		writer.write("------------------------------------------\r\n");
-		socketOut.write(template.setAlignCenter());
-		writer.write("欢迎品尝       谢谢惠顾\r\n");
-		socketOut.write(template.setAlignLeft());
+        writer.write("------------------------------------------\r\n");
+        socketOut.write(template.setAlignCenter());
+        writer.write("欢迎品尝       谢谢惠顾\r\n");
+        socketOut.write(template.setAlignLeft());
 
-		logger.info("------------------------");
-		logger.info("打印客用单，订单号：" + object.getOrderNo());
-		logger.info( "菜品明细："+ Arrays.toString(text));
-	}
+        logger.info("------------------------");
+        logger.info("[" + object.getCustomerPrinterIp() + "]打印客用单，订单号：" + object.getOrderNo());
+        logger.info("菜品明细：" + Arrays.toString(text));
+    }
 
-	public PrintData receiveMessage(PrintObj object , ListenerTemplate template) throws Exception {
-		return prepareData(object,new PrintData(),template);
-	}
+    public PrintData receiveMessage(PrintObj object, ListenerTemplate template) throws Exception {
+        return prepareData(object, new PrintData(), template);
+    }
 
 }
