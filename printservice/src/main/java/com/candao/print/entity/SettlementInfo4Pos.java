e965efd626104ad4f29c496a171f69a8d6d80509
caicai <837522223@qq.com>
2016-08-24 15:05:01 +0800

    修改结账单模板

--- printservice/src/main/java/com/candao/print/entity/SettlementInfo4Pos.java
+++ printservice/src/main/java/com/candao/print/entity/SettlementInfo4Pos.java
@@ -161,10 +161,10 @@ public class SettlementInfo4Pos implements Serializable {
             if (!StringUtils.isEmpty(orderinfo.getZdAmount()) && 1 == new BigDecimal(orderinfo.getZdAmount()).compareTo(new BigDecimal("0"))) {
                 settlementInfos.add(getSettlementInfo("赠送金额:", "￥" + orderinfo.getZdAmount()));
             }
-            float amount = new BigDecimal(dueamount).subtract(new BigDecimal(ssamount)).floatValue();
-            if (amount >= 0) {
-                settlementInfos.add(getSettlementInfo("总优惠:", "￥" + String.valueOf(amount)));
-            }
+//            float amount = new BigDecimal(dueamount).subtract(new BigDecimal(ssamount)).floatValue();
+//            if (amount >= 0) {
+            settlementInfos.add(getSettlementInfo("总优惠:", "￥" + (StringUtils.isEmpty(dueamount) ? "0.00" : dueamount)));
+//            }
             settlementInfos.add(getSettlementInfo("实收:：", "￥" + ssamount));
         }
 
