8d6d7662c37197656fd7bbc7267d99dec8443476
caicai <837522223@qq.com>
2016-09-09 10:31:46 +0800

    Rejected commit: Default

--- newspicyway/src/main/java/com/candao/www/webroom/service/impl/Print4POSServiceImpl.java
+++ newspicyway/src/main/java/com/candao/www/webroom/service/impl/Print4POSServiceImpl.java
@@ -434,7 +434,7 @@ public class Print4POSServiceImpl implements Print4POSService {
                         String[] name = {"合计：", resolveNullType(prefer.get("moneyWipeName")) + ":", "赠送金额:", "总优惠:", "应收:"};
                         String[] value = {resolveNullType(prefer.get("menuAmount")),
                                 resolveNullType(prefer.get("moneyWipeAmount")), resolveNullType(prefer.get("zdAmount")),
-                                stringAdd(resolveNullType(prefer.get("toalFreeAmount")),resolveNullType(prefer.get("moneyWipeName"))), resolveNullType(prefer.get("reserveAmout"))};
+                                stringAdd(resolveNullType(prefer.get("toalFreeAmount")),resolveNullType(prefer.get("moneyWipeAmount"))), resolveNullType(prefer.get("reserveAmout"))};
                         for (int i = 0; i < name.length; i++) {
                             Map<String, String> tempMap = new HashMap<>();
                             if (i > 0 && i < 3) {
