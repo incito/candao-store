1bcfe6442af3944525e3ad3a6ae0d7f6c57128d3
liaoying <360858909@qq.com>
2016-11-16 14:37:49 +0800

    服务费说明

--- newspicyway/src/main/java/com/candao/www/webroom/controller/PadInterfaceController.java
+++ newspicyway/src/main/java/com/candao/www/webroom/controller/PadInterfaceController.java
@@ -1081,12 +1081,52 @@ public class PadInterfaceController {
             int[] tabletypefilter = {2, 3, 4};
             map.put("tabletypefilter", tabletypefilter);// 过滤掉餐台类型为外卖,咖啡的餐台
             List<Map<String, Object>> list = tableService.find(map);
+            handleServiceCharge(list);
+
             return JacksonJsonMapper.objectToJson(ReturnMap.getSuccessMap(list));
         } catch (Exception e) {
             jsonString = JacksonJsonMapper.objectToJson(ReturnMap.getFailureMap("查询所有桌台异常"));
             logger.error("查询所有桌台异常！", e);
         }
         return jsonString;
+    }
+
+    /**
+     * 处理服务费说明
+     * @param list
+     */
+    private void handleServiceCharge(List<Map<String, Object>> list) {
+        for(Map<String, Object> table :list){
+            Object chargeOn=table.get("chargeOn");
+            short chargeOnShort = Short.valueOf(chargeOn.toString());
+            if(chargeOnShort == Constant.SERVICE_CHARGE_ON.OFF){
+                continue;
+            }
+            Object chargeType=table.get("chargeType");
+            if(null==chargeType){
+                continue;
+            }
+            switch (Short.valueOf(chargeType.toString()).shortValue()){
+                case Constant.SERVICE_CHARGE_TYPE.RATE:
+                    Object chargeRate = table.get("chargeRate");
+                    if(null!=chargeRate) {
+                        table.put("chargetDesc", "按菜品价格比例"+chargeRate+"%收取服务费");
+                    }
+                    break;
+                case Constant.SERVICE_CHARGE_TYPE.CONST:
+                    Object chargeAmount = table.get("chargeAmount");
+                    if(null!=chargeAmount) {
+                        table.put("chargetDesc", "收取固定服务费￥"+chargeAmount);
+                    }
+                    break;
+                case Constant.SERVICE_CHARGE_TYPE.TIME:
+                    Object chargeTime = table.get("chargeTime");
+                    if(null!=chargeTime) {
+                        table.put("chargetDesc", "按用餐时长收取服务费￥"+ table.get("chargeAmount")+"/"+chargeTime+"分钟");
+                    }
+                    break;
+            }
+        }
     }
 
     /**
